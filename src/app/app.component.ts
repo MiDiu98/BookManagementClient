bca1d14708459017ee4d6719672b7ea0446667bb
UNG MY DIEU <mydieu.mdu@gmail.com>
2020-12-05 11:46:13 +0700

    Restructure scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export interface DialogData {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'bookstore';
